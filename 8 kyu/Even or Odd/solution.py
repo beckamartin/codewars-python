bb74b935431a91970733b06c1d8ae403aa053f6c
Martin Bečka <becka.martin@icloud.com>
2023-02-06 19:11:13 +0100

    Modification of solution.py

--- 8 kyu/Even or Odd/solution.py
+++ 8 kyu/Even or Odd/solution.py
@@ -1,18 +1,3 @@
-import unittest
-
-
 def even_or_odd(number):
     if number % 2 == 0: return "Even"
     else: return "Odd"
-
-
-class TestEvenOrOdd(unittest.TestCase):
-    def test_even(self):
-        self.assertEqual(even_or_odd(2), "Even")
-
-    def test_odd(self):
-        self.assertEqual(even_or_odd(3), "Odd")
-
-        
-if __name__ == '__main__':
-    unittest.main()
